50b4527c05fac3a6a3185c25a9f5427a7f4446e0
Jean Vuillet <vuillet433@hotmail.com>
2024-02-18 15:38:56 -0500

    ok

--- FrontPart/src/__tests__/Bills.js
+++ FrontPart/src/__tests__/Bills.js
@@ -47,94 +47,95 @@ describe("Given I am connected as an employee", () => {
       expect(dates).toEqual(datesSorted)
     })
 
-    //demarage get bills
-
-    describe("Given I am a user connected as emplyee", () => {
-      describe("When I navigate to billList", () => {
-        test("fetches bills from mock API GET", async () => {
-          localStorage.setItem("user", JSON.stringify({ type: "employee", email: "a@a" }));
-          const root = document.createElement("div")
-          root.setAttribute("id", "root")
-          document.body.append(root)
-          router()
-          window.onNavigate(ROUTES_PATH.Bills)
-        
-          const contentPending  =  screen.getByText("Mes notes de frais")
-          expect(contentPending).toBeTruthy()
-          const arrayPending  =  screen.getByText("Type")
-          expect(arrayPending).toBeTruthy()
-
-        })
-      describe("When an error occurs on API", () => {
-        beforeEach(() => {
-          jest.spyOn(mockStore, "bills")
-          Object.defineProperty(
-              window,
-              'localStorage',
-              { value: localStorageMock }
-          )
-          window.localStorage.setItem('user', JSON.stringify({
-            type: 'Admin',
-            email: "a@a"
-          }))
-          const root = document.createElement("div")
-          root.setAttribute("id", "root")
-          document.body.appendChild(root)
-          router()
-        })
-        test("fetches bills from an API and fails with 404 message error", async () => {
-    
-          mockStore.bills.mockImplementationOnce(() => {
-            return {
-              list : () =>  {
-                return Promise.reject(new Error("Erreur 404"))
-              }
-            }})
-          window.onNavigate(ROUTES_PATH.Dashboard)
-          await new Promise(process.nextTick);
-          const message = await screen.getByText(/Erreur 404/)
-          expect(message).toBeTruthy()
-        })
-    
-        test("fetches messages from an API and fails with 500 message error", async () => {
-    
-          mockStore.bills.mockImplementationOnce(() => {
-            return {
-              list : () =>  {
-                return Promise.reject(new Error("Erreur 500"))
-              }
-            }})
-    
-          window.onNavigate(ROUTES_PATH.Bills)
-          await new Promise(process.nextTick);
-          const message = await screen.getByText(/Erreur 500/)
-          expect(message).toBeTruthy()
-        })
-        test('handleClickNewBill est appelée lorsque le bouton est cliqué', () => {
-          // Créer une fonction mock pour handleClickNewBill
-          const handleClickNewBill = jest.fn();
-        
-          // Créer des éléments de test simulés
-          const buttonNewBill = document.createElement('button');
-          buttonNewBill.setAttribute('data-testid', 'btn-new-bill');
-        
-          // Ajouter un écouteur d'événements sur le bouton
-          buttonNewBill.addEventListener('click', handleClickNewBill);
-        
-          // Ajouter le bouton au document
-          document.body.appendChild(buttonNewBill);
-        
-          // Simuler un clic sur le bouton
-          fireEvent.click(buttonNewBill);
-        
-          // Vérifier si handleClickNewBill a été appelée
-          expect(handleClickNewBill).toHaveBeenCalled();
-        });
-      })
-    
-      })
-    })
+   
     
     
   })
 })
+ //demarage get bills
+
+ describe("Given I am a user connected as emplyee", () => {
+    describe("When I navigate to billList", () => {
+      test("fetches bills from mock API GET", async () => {
+        localStorage.setItem("user", JSON.stringify({ type: "employee", email: "a@a" }));
+        const root = document.createElement("div")
+        root.setAttribute("id", "root")
+        document.body.append(root)
+        router()
+        window.onNavigate(ROUTES_PATH.Bills)
+      
+        const contentPending  =  screen.getByText("Mes notes de frais")
+        expect(contentPending).toBeTruthy()
+        const arrayPending  =  screen.getByText("Type")
+        expect(arrayPending).toBeTruthy()
+
+      })
+    describe("When an error occurs on API", () => {
+      beforeEach(() => {
+        jest.spyOn(mockStore, "bills")
+        Object.defineProperty(
+            window,
+            'localStorage',
+            { value: localStorageMock }
+        )
+        window.localStorage.setItem('user', JSON.stringify({
+          type: 'Admin',
+          email: "a@a"
+        }))
+        const root = document.createElement("div")
+        root.setAttribute("id", "root")
+        document.body.appendChild(root)
+        router()
+      })
+      test("fetches bills from an API and fails with 404 message error", async () => {
+  
+        mockStore.bills.mockImplementationOnce(() => {
+          return {
+            list : () =>  {
+              return Promise.reject(new Error("Erreur 404"))
+            }
+          }})
+        window.onNavigate(ROUTES_PATH.Dashboard)
+        await new Promise(process.nextTick);
+        const message = await screen.getByText(/Erreur 404/)
+        expect(message).toBeTruthy()
+      })
+  
+      test("fetches messages from an API and fails with 500 message error", async () => {
+  
+        mockStore.bills.mockImplementationOnce(() => {
+          return {
+            list : () =>  {
+              return Promise.reject(new Error("Erreur 500"))
+            }
+          }})
+  
+        window.onNavigate(ROUTES_PATH.Bills)
+        await new Promise(process.nextTick);
+        const message = await screen.getByText(/Erreur 500/)
+        expect(message).toBeTruthy()
+      })
+      test('handleClickNewBill est appelée lorsque le bouton est cliqué', () => {
+        // Créer une fonction mock pour handleClickNewBill
+        const handleClickNewBill = jest.fn();
+      
+        // Créer des éléments de test simulés
+        const buttonNewBill = document.createElement('button');
+        buttonNewBill.setAttribute('data-testid', 'btn-new-bill');
+      
+        // Ajouter un écouteur d'événements sur le bouton
+        buttonNewBill.addEventListener('click', handleClickNewBill);
+      
+        // Ajouter le bouton au document
+        document.body.appendChild(buttonNewBill);
+      
+        // Simuler un clic sur le bouton
+        fireEvent.click(buttonNewBill);
+      
+        // Vérifier si handleClickNewBill a été appelée
+        expect(handleClickNewBill).toHaveBeenCalled();
+      });
+    })
+  
+    })
+  })

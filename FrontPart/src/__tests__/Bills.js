86420ea9138b8515708a864eccaa83b5a9def246
Jean Vuillet <vuillet433@hotmail.com>
2024-06-20 08:54:28 -0500

    explication api erreur404

--- FrontPart/src/__tests__/Bills.js
+++ FrontPart/src/__tests__/Bills.js
@@ -147,6 +147,9 @@ describe("When I navigate to billList", () => {
         }
       }})
     window.onNavigate(ROUTES_PATH.Bills)
+    //on attend la promesse apres que le dome soit cree 
+    //(nextTick s execute apres le code synchrone
+    //await permet d attendre la resolution de la promesse
     await new Promise(process.nextTick);
     const message = await screen.getByText(/Erreur 404/)
     expect(message).toBeTruthy()
